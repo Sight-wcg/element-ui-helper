dfdf38cad241fc405f35a0ccd0cd1a5a13e09ae8
GitHub Action <action@github.com>
2022-10-02 17:14:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/sightwcg/elementuihelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sightwcg/elementuihelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sightwcg.elementuihelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sightwcg.elementuihelper.MyBundle
 
 class MyProjectService(project: Project) {
 
